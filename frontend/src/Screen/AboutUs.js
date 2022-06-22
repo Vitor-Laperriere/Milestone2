768631828098dcbdb3548d1ab96cb92e88f4779b
Pedro-C-Jardim <104333636+Pedro-C-Jardim@users.noreply.github.com>
2022-06-22 18:34:58 -0300

    aboutus

---
+++ frontend/src/Screen/AboutUs.js
@@ -0,0 +1,18 @@
+import {Link} from "react-router-dom";
+import React from "react";
+
+import ToolBar from "../components/Components-HomeScreen/ToolBar";
+import FootBar from "../components/Components-HomeScreen/FootBar";
+import Membros from "../components/Membros";
+
+
+function AboutUs(){
+    return(
+        <>
+            <ToolBar />
+            <Membros />
+            <FootBar/>
+        </>
+    );
+}
+export default AboutUs;
