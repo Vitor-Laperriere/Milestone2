2a0b778d39824a4d014f068d9a3d8f49c9445705
Pedro-C-Jardim <104333636+Pedro-C-Jardim@users.noreply.github.com>
2022-06-22 22:05:35 -0300

    Shipping Policy

---
+++ frontend/src/Screen/ShipPoli.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import ToolBar from "../components/Components-HomeScreen/ToolBar";
+import ShippingPolicy from "../components/ShippingPolicy";
+
+function ShipPoli(){
+    return(
+        <>
+            <ToolBar />
+            <ShippingPolicy />
+        </>
+    );
+}
+export default ShipPoli;
