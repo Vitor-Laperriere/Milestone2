f50bdf803f9ee7134c3262d7ff7ec45ee3027aa3
Pedro-C-Jardim <104333636+Pedro-C-Jardim@users.noreply.github.com>
2022-06-22 22:18:25 -0300

    Refund Policy

---
+++ frontend/src/components/RefundPolicy.js
@@ -0,0 +1,33 @@
+import './RefundPolicy.css'
+
+function RefundPolicy(){
+    return(
+        <section id="container">
+            
+        <div id="titulo">
+            <h1>Refund Policy</h1>
+        </div> 
+
+        <div class="desc_refund">
+            <p>Na JP²V, nosso compromisso é com a sua satisfação.</p><br></br>
+            <p>Nossa política de troca visa proporcionar ao cliente total segurança em relação aos produtos adquiridos em nossa loja.</p>
+            <p>Os produtos gratuitos não são passíveis de troca em nenhuma circunstância.</p>
+            <p>Todos nossos produtos possuem garantia contra defeitos de fabricação. Também realizamos trocas de produtos que vieram diferente daquele encomendado pelo cliente.</p>
+            <p>Caso você receba um produto nosso com algum defeito de fabricação ou diferente do que você ordenou siga os seguintes passos para realizar a troca:</p>
+            <br></br><li>Informe nossa equipe sobre o problema através de nosso e-mail, enviando fotos claras do produto com defeito ou do produto errado que você recebeu, no prazo de 3 dias após o recebimento. Atenção, não realizaremos trocas ou ressarcimentos após este prazo</li>
+            <br></br><li>A reclamação será analisada em no máximo 2 dias úteis. Constatado o problema, nossa equipe entrará em contato via e-mail instruindo como proceder com a troca ou a devolução do item (o custo de envio do produto para troca caso necessário é por conta do comprador).</li>
+            <br></br><li>Feito isso, você poderá escolher uma das seguintes alternativas:
+                <br></br><li>Receber o reembolso do valor pago</li>
+                <li>Receber um novo produto igual o encomendado</li>
+                <li>Receber um cupom com o valor do produto para utilizar em compras futuras</li><br></br>
+
+            </li>
+            <p>Não realizaremos trocas de produtos de tamanho inadequado para o cliente:</p>
+            <p>Disponibilizamos as medidas em centímetros de nossos produtos para não haver erro na escolha do tamanho. Segundo o Código de Defesa do Consumidor - Artigo 18, não existe a obrigação legal do lojista de realizar trocas por motivos de arrependimento do consumidor quanto à cor, tamanho, modelo, ou qualquer outra hipótese. O direito assegurado ao cliente pelo código de defesa do consumidor é o de realizar trocas apenas em caso de produtos com defeito.</p>
+        </div>
+        
+        </section>
+    );
+}
+
+export default RefundPolicy;
