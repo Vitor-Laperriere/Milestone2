dd6ec4cc33cf3380bbfed739ff2512e632f8c5a3
Vitor <vitorlape@usp.br>
2022-06-23 21:20:23 -0300

    toolbar arrumada

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,67 +3,25 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import HomeScreen from "./Screen/HomeScreen";
 import List from "./Screen/List";
-import Badge from 'react-bootstrap/Badge';
-import Nav from 'react-bootstrap/Nav';
+
 import ProductScreen from "./Screen/ProductScreen";
-import { useContext } from 'react';
-import { Store } from './Store';
-import FootBar from "./components/Components-HomeScreen/FootBar";
+
 import AboutUs from "./Screen/AboutUs";
 import CartScreen from "./Screen/CartScreen";
 import SigninScreen from './Screen/SigninScreen';
 import Help from "./Screen/Help";
 import ShipPoli from "./Screen/ShipPoli";
 import RefundPoli from "./Screen/RefundPoli";
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import { LinkContainer } from 'react-router-bootstrap'
+
 import ShippingAddressScreen from "./Screen/ShippingAddressScreen";
 import SignupScreen from "./Screen/SignupScreen";
 
-function App() {const { state, dispatch: ctxDispatch } = useContext(Store);
-  const { cart, userInfo } = state;
-
-  const signoutHandler = () => {
-    ctxDispatch({ type: 'USER_SIGNOUT' });
-    localStorage.removeItem('userInfo');
-    localStorage.removeItem('shippingAddress');
-  };
+function App() {
   return (
     <BrowserRouter>
     <div className="d-flex flex-column site-container">
         <ToastContainer position="bottom-center" limit={1} />
-      <Nav className="me-auto">
-                <Link to="/cart" className="nav-link">
-                  Cart
-                  {cart.cartItems.length > 0 && (
-                    <Badge pill bg="danger">
-                      {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
-                    </Badge>
-                  )}
-                </Link>
-                {userInfo ? (
-                  <NavDropdown title={userInfo.name} id="basic-nav-dropdown">
-                    <LinkContainer to="/profile">
-                      <NavDropdown.Item>User Profile</NavDropdown.Item>
-                    </LinkContainer>
-                    <LinkContainer to="/orderhistory">
-                      <NavDropdown.Item>Order History</NavDropdown.Item>
-                    </LinkContainer>
-                    <NavDropdown.Divider />
-                    <Link
-                      className="dropdown-item"
-                      to="#signout"
-                      onClick={signoutHandler}
-                    >
-                      Sign Out
-                    </Link>
-                  </NavDropdown>
-                ) : (
-                  <Link className="nav-link" to="/signin">
-                    Sign In
-                  </Link>
-                )}
-              </Nav>
+      
       <header >
         <main>
                 <Routes>
--- frontend/src/components/Components-HomeScreen/ToolBar.js
+++ frontend/src/components/Components-HomeScreen/ToolBar.js
@@ -1,16 +1,60 @@
 //import React from 'react';
 import { Link } from "react-router-dom";
 import './Toolbar.css'
+import Nav from 'react-bootstrap/Nav';
+import Badge from 'react-bootstrap/Badge';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { LinkContainer } from 'react-router-bootstrap'
+import { useContext } from 'react';
+import { Store } from "../../Store";
 
-function ToolBar(){
+function ToolBar(){const { state, dispatch: ctxDispatch } = useContext(Store);
+const { cart, userInfo } = state;
+
+const signoutHandler = () => {
+  ctxDispatch({ type: 'USER_SIGNOUT' });
+  localStorage.removeItem('userInfo');
+  localStorage.removeItem('shippingAddress');
+};
     return (
         <div className="tool-bar">
             <div className="container">
                 <Link to="/"><img src="./images/logoadasdasdasdasdasdasd.png" title="Logotipo" alt="logo_tropical" className="logotipo"/></Link>
                 <Link to={"/"} className="tool-link"><img src="./images/home.png" alt="home"className="ImgBar" width="51" height="51"/><p className="ImgBar">HOME</p></Link>
                 <Link to={"/list"} className="tool-link"><img src="./images/hand-watch.png" alt="hand watch"className="ImgBar" width="52" height="51"/><p>WATCHES</p></Link>
-                <Link to={"/cart"} className="tool-link"><img src="./images/shopping-cart.png" alt="cart"className="ImgBar" widht="51" height="51"/><p>CART</p></Link>
-                <Link to={"/signin"} className="tool-link"><img src="./images/user.png" alt="user"className="ImgBar" width="51" height="51"/><p>LOGIN</p></Link>
+
+              
+                    <Link to={"/cart"}  className="tool-link"><img src="./images/shopping-cart.png" alt="cart"className="ImgBar" widht="51" height="51"/>
+                    CART
+                    {cart.cartItems.length > 0 && (
+                        <Badge pill bg="danger">
+                        {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                        </Badge>
+                    )}
+                    </Link>
+                    {userInfo ? (
+                        <NavDropdown title={userInfo.name} id="basic-nav-dropdown">
+                        <LinkContainer to="/profile">
+                        <NavDropdown.Item>User Profile</NavDropdown.Item>
+                        </LinkContainer>
+                        <LinkContainer to="/orderhistory">
+                        <NavDropdown.Item>Order History</NavDropdown.Item>
+                        </LinkContainer>
+                        <NavDropdown.Divider />
+                        <Link  to="#signout"
+                        onClick={signoutHandler}
+                        className="tool-link"><img src="./images/user.png" alt="user"className="ImgBar" width="51" height="51"/>
+                        Sign Out
+                        </Link>
+                    </NavDropdown>
+                    ) : (
+                    <Link to="/signin"  className="tool-link"><img src="./images/user.png" alt="user"className="ImgBar" width="51" height="51"/>
+                    
+                        Sign In
+                    </Link>
+                    )}
+        
+                
             </div>
         </div>
         );
