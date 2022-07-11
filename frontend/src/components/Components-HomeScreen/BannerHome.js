e62ce394ad0d065217b7d0b1f9dd2c2070e6ee4e
João Matos <joao.deus@usp.br>
2022-07-11 17:17:24 -0300

    banner carrosel funcionando com easter egg

--- frontend/src/components/Components-HomeScreen/BannerHome.js
+++ frontend/src/components/Components-HomeScreen/BannerHome.js
@@ -1,11 +1,24 @@
-import './BannerHome.css'
+import Carousel from 'react-bootstrap/Carousel';
 
-function BannerHome(){
-    return(
-        <section id="banner_inicial">
-            <img src="./images/bannerWEB1.png" className="banner" alt='banner'/>
-        </section>
-    );
+function IndividualIntervalsExample() {
+  return (
+    <Carousel>
+      <Carousel.Item interval={2000}>
+        <img
+          className="d-block w-100"
+          src="./images/bannerSEMabalo2.png"
+          alt="Second slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="./images/bannerSEMabalo.png"
+          alt="Third slide"
+        />
+      </Carousel.Item>
+    </Carousel>
+  );
 }
 
-export default BannerHome;
+export default IndividualIntervalsExample;
